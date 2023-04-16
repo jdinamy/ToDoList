29395b36fb6939392a27ab9e2917e4ef8051fc94
jdinamy02 <noejdin@gmail.com>
2023-04-17 04:03:33 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,86 @@
+export const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+    slidesToSlide: 2,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+export const productData = [
+  {
+    id: 1,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/1.gif",
+    name: " Air Jordan",
+    price: "$416.99",
+    description: " Air Jordan 1 Retro High OG Chicago Lost and Found",
+  },
+  {
+    id: 2,
+    imageurl: "https://img.the-steppe.com/images/news/2657-jdcSvkfk.gif",
+    name: "Converse",
+    price: "$66.99",
+    description: "Converse высокие кеды Chuck Taylor All Star",
+  },
+  {
+    id: 3,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/2.gif",
+    name: "adidas ",
+    price: "$76.99",
+    description: "adidas кроссовки adidas Originals Stan Smith",
+  },
+  {
+    id: 4,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/9.gif",
+    name: "Nike",
+    price: "$286.99",
+    description: "Nike кроссовки Cortez Basic OG",
+  },
+  {
+    id: 5,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/6.gif",
+    name: "Onitsuka Tiger",
+    price: "$340.99",
+    description: "Onitsuka Tiger кроссовки Mexico 66 Deluxe",
+  },
+  {
+    id: 6,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/10.gif",
+    name: "PUMA",
+    price: "$167.99",
+    description: "Nike кроссовки Air Max 1 PRM Crepe Soft Grey",
+  },
+  {
+    id: 7,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/5.gif",
+    name: "adidas",
+    price: "$431.99",
+    description: "Junya Watanabe 574 из коллаборации с New Balance",
+  },
+  {
+    id: 8,
+    imageurl:
+      "https://the-steppe.com/pictures/files/Gulnar/05may/05classic/12.gif",
+    name: "Nike",
+    price: "$269.99",
+    description: "Nike кроссовки Air Force 1 Low",
+  },
+];
